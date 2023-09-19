14c77fc113013d0d209f4cbbfe513bfb6fe2311c
mewpk <milzaza123@gmail.com>
2023-09-19 19:13:35 +0700

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image"
+import { ProductDetail } from "../../components"
 
 const page = () => {
   return (
     <div className="main_container">
-      
+      <ProductDetail/>
     </div>
   )
 }
