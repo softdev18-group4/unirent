524bddab6cf3f3a1b36175af763aacdc11c29d4a
1tpp <panapat.dev@gmail.com>
2023-10-18 12:11:32 +0700

    chore: update

--- src/app/products/[id]/components/card/CardSeller.tsx
+++ src/app/products/[id]/components/card/CardSeller.tsx
@@ -1,17 +1,16 @@
-import { useAppSelector } from '@/redux/hooks';
-import { ProductState } from '@/types';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useAppSelector } from "@/redux/hooks";
+import { ProductState } from "@/types";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const CardSeller = () => {
-  const product : ProductState=  useAppSelector(
+  const product: ProductState = useAppSelector(
     (state: { productReducer: { value: any } }) => state.productReducer.value
   );
   const { data: session } = useSession();
-  const router = useRouter()
+  const router = useRouter();
 
   const handleCLick = async () => {
-
     const res = await fetch(`/api/services/conversation`, {
       method: "POST",
       headers: {
@@ -20,13 +19,13 @@ const CardSeller = () => {
       },
       body: JSON.stringify({
         userId: product.ownerId,
-      }), 
+      }),
     });
 
     const conversation = await res.json();
-    router.push(`/messages/${conversation.id}`)
-  }
-  
+    router.push(`/messages/${conversation.id}`);
+  };
+
   return (
     <div className="card_seller">
       <div className="left">
@@ -39,32 +38,37 @@ const CardSeller = () => {
         />
         <div className="avatar_title">
           {product && <h1>{product.owner.firstName}</h1>}
-            
-            <button onClick={handleCLick} className="btn_custom3 transition ease-in-out delay-150 duration-200 hover:scale-110 cursor-pointer text-white bg-[var(--theme-color2)" >แชทพูดคุย</button>
+
+          <button
+            onClick={handleCLick}
+            className="btn_custom3 transition ease-in-out delay-150 duration-200 hover:scale-110 cursor-pointer text-white bg-[var(--theme-color2)"
+          >
+            แชทพูดคุย
+          </button>
         </div>
       </div>
       <div className="line"></div>
       <div className="right">
-            <div className="item">
-                <h1>คะแนน</h1>
-                <p>3</p>
-            </div>
-            <div className="item">
-            <h1>รายการสินค้า</h1>
-                <p>2</p>
-            </div>
-            <div className="item">
-            <h1>อัตราการตอบกลับ</h1>
-                <p>94%</p>
-            </div>
-            <div className="item">
-            <h1>เวลาในการตอบกลับ</h1>
-                <p>ภายในไม่กี่ชั่วโมง</p>
-            </div>
-            <div className="item">
-            <h1>เข้าร่วมเมื่อ</h1>
-                <p>1 ปีที่ผ่านมา</p>
-            </div>
+        <div className="item">
+          <h1>คะแนน</h1>
+          <p>3</p>
+        </div>
+        <div className="item">
+          <h1>รายการสินค้า</h1>
+          <p>2</p>
+        </div>
+        <div className="item">
+          <h1>อัตราการตอบกลับ</h1>
+          <p>94%</p>
+        </div>
+        <div className="item">
+          <h1>เวลาในการตอบกลับ</h1>
+          <p>ภายในไม่กี่ชั่วโมง</p>
+        </div>
+        <div className="item">
+          <h1>เข้าร่วมเมื่อ</h1>
+          <p>1 ปีที่ผ่านมา</p>
+        </div>
       </div>
     </div>
   );
